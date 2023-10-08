e56ac7bb5e3fb612df390a3e754460368bb6e5f7
Bella <bella.kallen2014@gmail.com>
2023-10-09 02:49:17 +0300

    src added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        int balance = 100;
+        int amount = 1100;
+        int amountBalance = balance + amount;
 
-}
+        if (amountBalance < 1000) {
+            int allBalance = balance + amount;
+            System.out.println(" Баланс: " + allBalance);
+            System.out.println(" Кол-во бонусных рублей: 0");
+        }
+        else {
+                int bonus = 100;
+                int allBonus = amount / bonus;
+                int allBalance2 = balance + amount + allBonus;
+            System.out.println(" Баланс: " + allBalance2);
+            System.out.println(" Кол-во бонусных рублей: " + allBonus);
+            }
+        }
+    }
